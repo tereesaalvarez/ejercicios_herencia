8255824957fdcb4344ee6deaa5579e3dddbeb4b6
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-21 18:15:35 +0100

    commit

--- clases/herencia_diamante.py
+++ clases/herencia_diamante.py
@@ -1,11 +1,3 @@
-#Importamos la libreria funciones
-import funciones
-#Definimos el main
-if __name__ == "__main__":
-    d = funciones.D(1, 2, 3)
-    print(isinstance(d, funciones.A),isinstance(d, funciones.B),isinstance(d, funciones.C))
-    print(d.a, d.b, d.c) 
-    
 class A:
     def __init__(self, a):
         self.a = a
