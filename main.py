7d8cfd33ef85e731fd4f9ec3233253b8fbbb7c90
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-21 18:21:57 +0100

    commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from tkinter import N
 from clases.herencia_simple import *
 from clases.herencia_real import *
+from clases.herencia_diamante import *
 
 # Ejercicio 1: Herencia Simple
 
@@ -11,6 +13,68 @@ if __name__ == '__main__':
     punto2 = Punto3D(5,3,7)
     punto2.translacion(2,2,3)
     print("Las coordenadas del punto nuevo son ({},{})".format(punto2.x, punto2.y, punto2.z))
+
+
+# Ejercicio 2: Puzzle
+
+class Base: 
+    def __init__(self): 
+        self.a = "a" 
+        self.b = "b" 
+        self.c = "c" 
+    def A(self): 
+        print(self.a) 
+ 
+    def B(self): 
+        print(self.b) 
+ 
+    def C(self): 
+        print(self.c) 
+
+#Una vez definida la clase Base, definimos la clase que deriva de dicha base igual que la anterior, haciendo uso de las herencias
+class Derivada(Base): 
+    def __init__(self): 
+        self.a = "aa" 
+        super().__init__() 
+        self.c = "cc" 
+    def A(self): 
+        print(self.a) 
+    def B(self): 
+        self.b = "bb" 
+        super().B() 
+        print(self.b) 
+#El resultado es el siguiente:
+'''
+a
+b
+bb
+bb
+c
+cc
+c
+'''
+
+
+# Ejercicio 3: Herencia diamante 
+class A:
+    def __init__(self, a):
+        self.a = a
+
+class B(A):
+    def __init__(self, a, b):
+        A.__init__(self, a)
+        self.b = b
+        
+class C(A):
+    def __init__(self, a, c):
+        A.__init__(self, a)
+        self.c = c
+
+class D(B, C):
+    def __init__(self, a, b, c):
+        B.__init__(self, a, b)
+        C.__init__(self, a, c)
+
 
 
 
