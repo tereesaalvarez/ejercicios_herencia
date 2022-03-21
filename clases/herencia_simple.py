8dbe7cff0a82b152cd7f1115e5902df77c953e15
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-21 17:11:26 +0100

    definido constructor

--- clases/herencia_simple.py
+++ clases/herencia_simple.py
@@ -3,7 +3,8 @@ from tkinter import Y
 
 class Punto2D:
     def __init__(self, x, y):
-        self.y= Y
+        self.y= y
         self.x= x
     def translacion(self, a, b):
-        
+        self.y = self.y + b
+        self.x = self.x + a
