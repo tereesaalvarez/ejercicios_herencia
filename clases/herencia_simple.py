2c70d53deac6a13bd6f1faa5610a59d3cebefd56
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-21 17:13:57 +0100

    clase 3d

--- clases/herencia_simple.py
+++ clases/herencia_simple.py
@@ -8,3 +8,8 @@ class Punto2D:
     def translacion(self, a, b):
         self.y = self.y + b
         self.x = self.x + a
+
+class Punto3D(Punto2D):
+    def __init__(self, x, y, z):
+        Punto2D.__init__(self, x, y)
+        self.z = z
