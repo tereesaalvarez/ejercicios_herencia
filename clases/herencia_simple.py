81feb0b308af252cdd27f8bae26db862a33ae338
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-21 17:15:37 +0100

    commit

--- clases/herencia_simple.py
+++ clases/herencia_simple.py
@@ -3,13 +3,20 @@ from tkinter import Y
 
 class Punto2D:
     def __init__(self, x, y):
-        self.y= y
         self.x= x
+        self.y= y
+        
     def translacion(self, a, b):
-        self.y = self.y + b
         self.x = self.x + a
+        self.y = self.y + b
+        
 
 class Punto3D(Punto2D):
     def __init__(self, x, y, z):
         Punto2D.__init__(self, x, y)
         self.z = z
+
+    def translacion2(self, a, b, c):
+        self.x = self.x + a
+        self.y = self.y + b
+        self.z = self.z + c
