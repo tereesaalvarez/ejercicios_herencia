7a71afa0b83e8c0cc7a8e9251813b765f7f73c24
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-21 17:46:52 +0100

    definimos la clase que depende de la anterior

--- clases/puzzle.py
+++ clases/puzzle.py
@@ -20,3 +20,15 @@ class Base:
     def C(self): 
         print(self.c) 
 
+#Una vez definida la clase Base, definimos la clase que deriva de dicha base igual que la anterior, haciendo uso de las herencias
+class Derivada(Base): 
+    def __init__(self): 
+        self.a = "aa" 
+        super().__init__() 
+        self.c = "cc" 
+    def A(self): 
+        print(self.a) 
+    def B(self): 
+        self.b = "bb" 
+        super().B() 
+        print(self.b) 
