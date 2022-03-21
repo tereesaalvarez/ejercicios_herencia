afac8ae9155acb50b98af7a8b144f71f4ddb34ab
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-03-21 17:43:10 +0100

    definimos las clases y los respondientes constructores para A, B y C

--- clases/puzzle.py
+++ clases/puzzle.py
@@ -5,4 +5,18 @@ if __name__ == "__main__":
     d = funciones.D(1, 2, 3)
     print(isinstance(d, funciones.A),isinstance(d, funciones.B),isinstance(d, funciones.C))
     print(d.a, d.b, d.c)    
-    
+#Empezamos a definir las clases y definimos los constructores para A, B y C
+class Base: 
+    def __init__(self): 
+        self.a = "a" 
+        self.b = "b" 
+        self.c = "c" 
+    def A(self): 
+        print(self.a) 
+ 
+    def B(self): 
+        print(self.b) 
+ 
+    def C(self): 
+        print(self.c) 
+
